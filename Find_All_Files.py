dcfc0f10a71b0e68fbc3c975a18519efe57d7ffb
Richard Smith <smithrls2002@gmail.com>
2026-02-10 22:59:29 -0500

    Updated the ReadMe

--- Find_All_Files.py
+++ Find_All_Files.py
@@ -1,4 +1,5 @@
 from os import walk
+import os.path
 
 
 def response_valid(response, valid_responses):
@@ -9,11 +10,23 @@ def response_valid(response, valid_responses):
             return True
     return False
 
+def validate_string(msg, valid_responses, alternative_response, default_state, alternative_state):
+    while(True):
+        temp = input(msg+" ").lower()
+        if(not(response_valid(temp, valid_responses))):
+            print("ERROR: INVALID RESPONSE INPUT")
+            continue
+        if(temp[0] == alternative_response):
+            return alternative_state
+        return default_state
+
 def main():
     VALID_BIN_RESPONSES = ("y", "n", "yes", "no")
+    VALID_PRINT_OPTIONS = ("console", "file")
+    VALID_SEPARATORS = ("space", "newline", "new line")
     path = input("PLEASE INPUT THE PATH TO THE DESIRED DIRECTORY\n").replace("/", "\\")
     type = ""
-    remove_root = False
+    console = True
 
     while(True):
         type = input("PLEASE INPUT THE DESIRED FILE TYPE\n")
@@ -24,14 +37,20 @@ def main():
             break
         print("ERROR: PROVIDED FILE TYPE IS INVALID.")
 
+    remove_root = validate_string("DO YOU WITH TO INCLUDE THE ROOT? (Y/N)", VALID_BIN_RESPONSES, 'n', False, True)
+
+    separator = validate_string("SEPARATE WITH A SPACE OR A NEW LINE", VALID_SEPARATORS, 'n', " ", "\n")
+
     while(True):
-        temp = input("DO YOU WISH TO INCLUDE THE ROOT? (Y/N) ").lower()
-        if(not(response_valid(temp, VALID_BIN_RESPONSES))):
+        temp = input("DO YOU WISH TO PRINT TO THE CONSOLE OR A FILE? ").lower()
+        if(not(response_valid(temp, VALID_PRINT_OPTIONS))):
             print("ERROR: INVALID RESPONSE INPUT")
             continue
-        if(temp[0] == 'n'):
-            remove_root = True
+        if(temp == "file"):
+            console = False
         break
+
+    print_progress = validate_string("DO YOU WISH TO PRINT OUT DIRECTORIES AND THEIR FILES AS THEY ARE FOUND? (Y/N)", VALID_BIN_RESPONSES, 'n', True, False)
 
     directoryPaths = []
     fileNames = []
@@ -51,10 +70,26 @@ def main():
         for i in range(len(directoryPaths)):
             directoryPaths[i] = directoryPaths[i].replace(path, "", 1)
         
-
     for i in range(len(directoryPaths)):
-        print(directoryPaths[i]+":")
-        print(fileNames[i])
+        if(print_progress):
+            print(directoryPaths[i]+":")
+            print(fileNames[i])
+            print()
+
+    if(console):
         print()
+        return
+    
+    modify = False
+    while(not(modify)):
+        out_file = input("PLEASE INPUT THE NAME OF THE OUTPUT FILE: ")
+        if(os.path.isfile(out_file)):
+            print("FILE ALREADY EXISTS", " ")
+            modify = validate_string("DO YOU WISH TO OVERWRITE IT? (Y/N)", VALID_BIN_RESPONSES, 'n', True, False)
+        else:
+            modify = True
+    return
+
+
 
 main()
--- ReadMe.md
+++ ReadMe.md
@@ -1,6 +1,25 @@
 This program was written in Python 3.1.3 using Visual Studio Code
 <br>
 <br>
-When you run it, you will be prompted to input a directory root, a desired file type, and if you wish to include the<br>
-root that you gave (and it will eventually also ask if you wish to print to a file or just to the console<br>
-and if you want to print the files found and their directories to the console)
+When you run it, you will be prompted to provide several options. Then, the program will generate a list of files
+<br>
+relative to a path and print it either to the console or a file.
+<br>
+If you choose to print to a file, you will be prompted to provide a name for the file after the string is created
+<br>
+If a file by the provided name already exists, you will be asked if you wish to overwrite it
+<br>
+<br>
+Subdirectories that do not contain any files of interest will not be included
+<br>
+<br>
+The options are, in order:
+<br>
+<ul>
+    <li>The root of the subdirectory you wish to to explore</li>
+    <li>The file type of interest (A blank space or "all" will return all files)</li>
+    <li>Whether or not you wish to include the root</li>
+    <li>What type of seperator you would like (space or new line)</li>
+    <li>Whether or not you want to save to a file or print to the console</li>
+    <li>Whether or not you would like to print the subdirectories and their files to the console as it constructs the string</li>
+</ul>
